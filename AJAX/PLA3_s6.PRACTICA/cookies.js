59751712e8e34d619b5c9da1f4e79eeeca68b9c2
admin CIFO <admin CIFO@DESKTOP-240OAG0>
2019-10-28 15:42:33 +0100

    Datatable

---
+++ AJAX/PLA3_s6.PRACTICA/cookies.js
@@ -0,0 +1,74 @@
+
+/**
+ * Crea una cookie con los valores de los parametros
+ * 
+ * @param string  name Nombre de la Cookie
+ * @param string  value Valor de la Cookie
+ * @param integer expires Meses de validez de la cookie
+ * @param string path Ruta de validez de la Cookie; Ejemplo '/'
+ * @param string domain Dominio de validez de la cookie ; Ejemplo :''
+ * @returns none
+ */
+function createCookie(name, value, expires, path, domain) {
+  var curdate = new Date();
+  curdate.setMonth(curdate.getMonth() + expires);
+  var cookieExpires = curdate.toUTCString();
+  var txtCookie = name + "=" + encodeURIComponent(value) + "; expires= "
+      + cookieExpires + ";path=" + path + "; domain=" + domain;
+  document.cookie = txtCookie;
+}
+
+/**
+ * Leer el contenido de una cookie
+ * 
+ * @param string name Nombre de la Cookie
+ * @returns boolean Falso si no existe la cookie
+ */
+
+function getCookie(name) {
+  var search_cookie = name + "=";
+  if (document.cookie.length > 0) {
+    var start_position = document.cookie.indexOf(search_cookie)
+    if (start_position != -1) {
+      start_position += search_cookie.length
+      var end_position = document.cookie.indexOf(";", start_position)
+      if (end_position == -1){
+        end_position = document.cookie.length;
+      }
+      return (decodeURIComponent(document.cookie.substring(
+          start_position, end_position)))
+    } else {
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+/**
+ * Elimina una Cookie determinada
+ * 
+ * @param string name Nombre de la Cookie
+ * @param string path Ruta de validez de la Cookie; Ejemplo '/'
+ * @returns
+ */
+function deleteCookie(name,path){
+  createCookie(name,"",-1,path,"");
+}
+
+document.querySelector("#cookiePanel button").onclick=function(){
+      
+      createCookie("cOk","ok",1,"/",""); // (nombre,valor,expire,path,domain )
+      window.sessionStorage.setItem("sOk","ok");
+      document.getElementById("cookiePanel").style.display="none";
+}
+    
+document.getElementById("spanCookiePanel").onclick=function(){
+      document.getElementById("cookiePanel").style.display="none";
+}
+    
+    var cOk=getCookie("cOk");
+    var sOk = window.sessionStorage.getItem("sOk");
+    if(cOk=="ok" && sOk=="ok"){
+      document.getElementById("cookiePanel").style.display="none";
+}
---
+++ AJAX/PLA3_s6.PRACTICA/server.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php 
+
+sleep(2)
+
+?>
+</body>
+</html>
+
+<?php 
+
+sleep(2)
+
+?>
